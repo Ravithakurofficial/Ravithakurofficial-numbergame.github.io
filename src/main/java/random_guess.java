3b4def6fb90f192fdedd4cd666bd9f3e7d77d6b9
Ravi kumar Thakur <thakurravikumar400@gmail.com>
2023-08-08 10:05:47 +0530

    value coming properly

--- src/main/java/random_guess.java
+++ src/main/java/random_guess.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 @WebServlet("/random_guess")
 public class random_guess extends HttpServlet {
@@ -20,17 +21,34 @@ public class random_guess extends HttpServlet {
 
         String minStr = request.getParameter("Min");
         String maxStr = request.getParameter("Max");
+        String Userstr = request.getParameter("userguess");
         
         try {
             int min = Integer.parseInt(minStr);
             int max = Integer.parseInt(maxStr);
+            int userin = Integer.parseInt(Userstr);
+            
             
             pw.println("<h1>Welcome to the Guessing Game!</h1>");
-            pw.println("Entered minimum number: " + min);
-            pw.println("Entered maximum number: " + max);
+            //pw.println("Entered minimum number: " + min);
+            //pw.println("Entered maximum number: " + max);
+            //pw.println("user guess"+userin);
+            Random randomValue = new Random();
+            int random = randomValue.nextInt(max - min + 1) + min;
+            pw.println("Secret number is " +random);
             
-            // Perform any other logic here
-            
+            if(random != userin && random > userin){
+            	pw.println("your guess is low");
+            }
+            else if(random != userin && random < userin) {
+            	pw.println("Your guess is high");
+            }
+            else if(random == userin) {
+            	pw.println("Your guess is correct");
+            }
+            else {
+            	pw.println("other problem");
+            }
         } catch (NumberFormatException e) {
             pw.println("<p>Invalid input. Please enter valid numbers.</p>");
         }

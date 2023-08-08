2b33807f5169103ede24d6024126c09160121466
Ravi kumar Thakur <thakurravikumar400@gmail.com>
2023-08-08 09:33:05 +0530

    ajax implementation done

--- src/main/java/random_guess.java
+++ src/main/java/random_guess.java
@@ -1,5 +1,3 @@
-
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,39 +6,33 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
-@WebServlet("/index.html")
-
-/**
- * Servlet implementation class Random_guess
- */
-public class Random_guess extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@WebServlet("/random_guess")
+public class random_guess extends HttpServlet {
+    private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Random_guess() {
+    public random_guess() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			PrintWriter pw = response.getWriter();
-	        String min = request.getParameter("Min");
-	        String max = request.getParameter("Max");
-	        
-	        int mi = Integer.parseInt(min);
-	        int ma = Integer.parseInt(max);
-	        
-	        pw.println("enter numbers are "+mi);
-	        pw.println("enter numbers are "+ma);
-	        
-	        
-	        
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
 
+        String minStr = request.getParameter("Min");
+        String maxStr = request.getParameter("Max");
+        
+        try {
+            int min = Integer.parseInt(minStr);
+            int max = Integer.parseInt(maxStr);
+            
+            pw.println("<h1>Welcome to the Guessing Game!</h1>");
+            pw.println("Entered minimum number: " + min);
+            pw.println("Entered maximum number: " + max);
+            
+            // Perform any other logic here
+            
+        } catch (NumberFormatException e) {
+            pw.println("<p>Invalid input. Please enter valid numbers.</p>");
+        }
+    }
 }
